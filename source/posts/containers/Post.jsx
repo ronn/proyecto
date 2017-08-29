8c93e167851b4d683fb1f1dc1a72522c2b42e533
ronn <ronniecanas@seven4n.com>
2017-08-29 16:25:19 -0500

    CombineReducers implemented

--- source/posts/containers/Post.jsx
+++ source/posts/containers/Post.jsx
@@ -1,10 +1,12 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
-
-import api from '../../api'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import styles from './Post.css'
+
+import actions from '../../actions'
 
 class Post extends Component{
     constructor(props){
@@ -22,28 +24,17 @@ class Post extends Component{
     }
 
     async initialFetch(){
-        if (this.state.user && this.state.comments){
-            return this.setState({
-                loading: false
-            })
-        }
+        if (!!this.props.user && !!this.props.comments) return this.setState({ loading: false })
 
-        const [
-            user,
-            comments
-        ] = await Promise.all([
-            !this.state.user ? api.users.getSingle(this.props.userId) : Promise.resolve(null),
-            !this.state.comments ? api.posts.getCommments(this.props.id) : Promise.resolve(null),
+        await Promise.all([
+            this.props.actions.loadUser(this.props.iserId),
+            this.props.actions.loadComments(this.props.id)
         ])
 
-        return this.setState({
-            loading: false,
-            user: user || this.state.user,
-            comments: comments || this.state.comments
-        })
+        return this.setState({ loading: false })
     }
 
-    render(){
+    render() {
         return(
             <article id={`post-${this.props.id}`} className={styles.post}>
                 <h2 className={styles.title}>
@@ -56,15 +47,15 @@ class Post extends Component{
                 </p>
                 {!this.state.loading && (
                     <div className={styles.meta}>
-                        <Link to={`/user/${this.state.user.id}`} className={styles.user}>
-                            {this.state.user.name}
+                        <Link to={`/user/${this.props.user != null ? this.props.user.id : 0}`} className={styles.user}>
+                            {this.props.user != null ? this.props.user.name : ''}
                         </Link>
 
                         <span className={styles.comments}>
                             <FormattedMessage
                                 id="post.meta.comments"
                                 values={{
-                                    amount: this.state.comments.length
+                                    amount: this.props.comments.length
                                 }}
                             />
                         </span>
@@ -78,11 +69,15 @@ class Post extends Component{
     }
 }
 
-Post.prpTypes = {
-    id: PropTypes.number,
-    userId: PropTypes.number,
-    title: PropTypes.string,
-    body: PropTypes.string
-}
+const mapStateToProps = (state, props) => ({
+    comments: state.comments != null
+        ? state.comments.filter(comment => comment.postId === props.id)
+        : [],
+    user: state.users[props.userId]
+})
 
-export default Post
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(actions, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
--- source/reducer.js
+++ source/reducer.js
@@ -1,3 +1,8 @@
+import { combineReducers } from 'redux'
+
+const SET_POST = 'SET_POST'
+const SET_COMMENTS = 'SET_COMMENTS'
+const SET_USER = 'SET_USER'
 
 const initialState = {
     posts: {
@@ -8,18 +13,53 @@ const initialState = {
     users: {}
 }
 
-const reducer = (state = initialState, action = {}) => {
+const postsPageReducer = (state = initialState.posts.page, action = {}) => {
     switch (action.type){
-    case 'SET_POST':
+    case SET_POST:
+        return state + 1
+    default:
+        return state
+    }
+}
+
+const postEntitiesReducer = (state = initialState.posts.entities, action = {}) => {
+    switch (action.type){
+    case SET_POST:
+        return state.concat(action.payload)
+    default:
+        return state
+    }
+}
+
+const postReducer = combineReducers({
+    page: postsPageReducer,
+    entities: postEntitiesReducer
+})
+
+const commentsReducer = (state = initialState.comments, action = {}) => {
+    switch (action.type){
+    case SET_COMMENTS:
+        return state.concat(action.payload)
+    default:
+        return state
+    }
+}
+
+const userReducer = (state = initialState.users, action = {}) => {
+    switch (action.type){
+    case SET_USER:
         return Object.assign({}, state, {
-            posts: Object.assign({}, state.posts, {
-                entities: state.posts.entities.concat(action.payload),
-                page: state.posts.page + 1
-            })
+            [action.payload.id]: action.payload
         })
     default:
         return state
     }
 }
+
+const reducer = combineReducers({
+    posts: postReducer,
+    commets: commentsReducer,
+    users: userReducer
+})
 
 export default reducer
